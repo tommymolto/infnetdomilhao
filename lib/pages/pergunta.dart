7921436c8e36d261301544128cb880e63d6f9b98
Paulo Marinho <tommymolto@gmail.com>
2022-05-04 21:20:21 -0300

    Pratica aula 4

--- lib/pages/pergunta.dart
+++ lib/pages/pergunta.dart
@@ -4,36 +4,63 @@ import 'package:flutter/material.dart';
 import 'package:infnetdomilhao/pages/resposta.dart';
 
 class Pergunta extends StatelessWidget {
-  const Pergunta({Key? key}) : super(key: key);
+  final String pergunta;
+  final List<String> respostas;
+  final int indice;
+  final Function onClick;
+
+  const Pergunta(
+      {Key? key,
+      required this.pergunta,
+      required this.respostas,
+      required this.indice,
+      required this.onClick})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var respostas = [
-      'RIO DE JANEIRO',
-      'RIO GRANDE DO SUL',
-      'SANTA CATARINA',
-      'GOIÁS',
-    ];
     respostas.shuffle();
-    const indice = 0;
+
     return Center(
       child: Container(
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const Expanded(
+              Expanded(
                 flex: 3,
-                child: Text(
-                    'Em que estado brasileiro nasceu a apresentadora Xuxa ? '),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    pergunta,
+                    style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      // backgroundColor: Colors.cyanAccent.withOpacity(0.5),
+                    ),
+                  ),
+                ),
               ),
               Expanded(
                   flex: 1,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Resposta(textoResposta: respostas[0].toString(), iconData: Icons.circle, function: _onClicked),
-                      Resposta(textoResposta: respostas[1].toString(), iconData: Icons.square, function: _onClicked),
+                      Resposta(
+                        textoResposta: respostas[0].toString(),
+                        iconData: Icons.circle,
+                        function: onClick,
+                        indice: indice,
+                        cor: Colors.cyanAccent,
+                      ),
+                      Resposta(
+                        textoResposta: respostas[1].toString(),
+                        iconData: Icons.square,
+                        function: onClick,
+                        indice: indice,
+                        cor: Colors.yellow,
+                      ),
                     ],
                   )),
               Expanded(
@@ -42,19 +69,25 @@ class Pergunta extends StatelessWidget {
                     child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Resposta(textoResposta: respostas[2].toString(), iconData: Icons.star, function: _onClicked),
-                    Resposta(textoResposta: respostas[3].toString(), iconData: Icons.add, function: _onClicked),
-
+                    Resposta(
+                      textoResposta: respostas[2].toString(),
+                      iconData: Icons.star,
+                      function: onClick,
+                      indice: indice,
+                      cor: Colors.deepOrange,
+                    ),
+                    Resposta(
+                      textoResposta: respostas[3].toString(),
+                      iconData: Icons.add,
+                      function: onClick,
+                      indice: indice,
+                      cor: Colors.lightGreenAccent
+                    ),
                   ],
                 )),
               )
             ],
           )),
     );
-  }
-
-  _onClicked(String valor) {
-    print(valor);
-    //stdout.writeln(valor);
   }
 }
--- lib/pages/resposta.dart
+++ lib/pages/resposta.dart
@@ -5,10 +5,16 @@ class Resposta extends StatelessWidget {
   final String textoResposta;
   final IconData iconData;
   final Function function;
+  final Color cor;
+  final int indice;
 
-  const Resposta(
-      {Key? key, required this.textoResposta, required this.iconData, required this.function,})
-      : super(key: key);
+  const Resposta({
+    Key? key,
+    required this.textoResposta,
+    required this.iconData,
+    required this.function,
+    required this.indice, required this.cor,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +23,29 @@ class Resposta extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(4.0),
         child: ElevatedButton.icon(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(cor),
+            maximumSize:  MaterialStateProperty.all(const Size(22, 48)),
+            minimumSize:  MaterialStateProperty.all(const Size(22, 48)) ,
+          ),
           onPressed: () {
-            function(textoResposta);
+            function(textoResposta, indice);
             //function(textoResposta);
           },
-          label: Text(textoResposta),
+          label: Text(textoResposta,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                // backgroundColor: cor,
+              )),
           icon: Icon(
-              iconData
+              iconData,
+              size: 44.0,
+            color: Colors.black,
           ),
         ),
       ),
     );
   }
-
 }
